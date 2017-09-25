71941bb60c046476350de2c01d453a851e5f999b
jackson <jackson@jackson-GP62-2QD>
2017-09-25 09:48:30 -0400

    Instantiated address book and tested add and remove buddy.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,12 +6,17 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		System.out.println("Address book");
+		BuddyInfo buddy = new BuddyInfo();
+		AddressBook addressBook = new AddressBook();
+		addressBook.addBuddy(buddy);
+		addressBook.removeBuddy(buddy);
 	}
 	
 	public void addBuddy(BuddyInfo buddyInfo) {
 		buddyList.add(buddyInfo);
 	}
-	public void removeBuddy() {
+	public void removeBuddy(BuddyInfo buddyInfo) {
+		buddyList.remove(buddyInfo);
 	}
 
 }
