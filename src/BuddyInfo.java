332eed47825efe0a12c89e7b02279903461d81c8
jackson <jackson@jackson-GP62-2QD>
2017-10-02 09:34:34 -0400

    yes

---
+++ src/BuddyInfo.java
@@ -0,0 +1,32 @@
+
+public class BuddyInfo {
+
+	String name;
+	String address;
+	String phoneNumber;
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		BuddyInfo buddy = new BuddyInfo();
+		buddy.setName("Homer");
+		System.out.println("Hello "+buddy.getName());
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
