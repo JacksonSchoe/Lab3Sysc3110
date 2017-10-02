964e1228e1401c39e7f5d0491b160ea9bd108094
jackson <jackson@jackson-GP62-2QD>
2017-10-02 09:02:38 -0400

    added words

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -11,7 +11,7 @@ public class AddressBook {
 		addressBook.addBuddy(buddy);
 		addressBook.removeBuddy(buddy);
 	}
-	
+	kjn
 	//default address book constructor
 	public AddressBook() {
 		this.buddyList = new ArrayList<BuddyInfo>();
